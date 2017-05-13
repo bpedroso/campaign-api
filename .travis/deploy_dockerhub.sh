452441d3f2de93478b77090acec0cedaca666633
bpedroso <pedroso.bruno@gmail.com>
2017-05-13 18:56:08 -0300

    Add trace on travisci

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-
-docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-#docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+echo "docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS"
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
+
+echo "docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG ."
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG
